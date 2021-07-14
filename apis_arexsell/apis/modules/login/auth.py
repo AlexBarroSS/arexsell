aced63208d2dd3360c6826218b309508ad65fb64
Alex Barros <alex.silva123@fatec.sp.gov.br>
2021-07-13 21:43:17 -0300

    Created UserLogin class, Create, Read and Update

--- apis_arexsell/apis/modules/login/auth.py
+++ apis_arexsell/apis/modules/login/auth.py
@@ -1,19 +1,79 @@
+import bcrypt
+from django.core.validators import validate_email
+
 from apis.models import User
 
 
 class UserLogin:
     def __init__(self, name, password, email):
         self.name = name
-        self.password = password
+        self.password = UserLogin.encrypt_password(password)
         self.email = email
 
     def register_user(self):
-        model_user = User.objects.create(
+        user = User.objects.create(
             name=self.name,
             password=self.password,
             email=self.email
         )
 
-        model_user.save()
+        try:
+            validate_email(user.email)
+            user.save()
 
-        return model_user.name
+            return {'success': True, 'user': UserLogin.user_to_json(user)}
+
+        except:
+            return {'success': False, 'user': None}
+
+    def update_user(self):
+        try:
+            validate_email(self.email)
+
+            user = User.objects.get(name=self.name)
+
+            user.email = self.email
+            user.password = self.password
+            user.save()
+
+            return {'success': True, 'user': UserLogin.user_to_json(user)}
+        except:
+            return {'success': False, 'user': None}
+
+    @staticmethod
+    def get_user_by_name(name):
+        try:
+            user = User.objects.get(name=name)
+            return {
+                'success': True,
+                'user': UserLogin.user_to_json(user)
+            }
+        except:
+            return {
+                'success': False,
+                'user': None
+            }
+
+    def is_correct_password(self, password):
+        try:
+            if bcrypt.checkpw(str.encode(password), str.encode(self.password)):
+                return True
+            return False
+        except:
+            return False
+
+    @staticmethod
+    def encrypt_password(password):
+        try:
+            hashed_password = bcrypt.hashpw(
+                str.encode(password), bcrypt.gensalt())
+            return hashed_password.decode("utf-8")
+        except:
+            return False
+
+    @ staticmethod
+    def user_to_json(user):
+        return {
+            "name": user.name,
+            "email": user.email
+        }
--- apis_arexsell/apis/tests.py
+++ apis_arexsell/apis/tests.py
@@ -1,41 +1,87 @@
 
-from django.db import models
+import random
 from django.test import TestCase
 
 from apis.modules.login.auth import UserLogin
-from apis.models import User
 
-default_user = {
+user_json = {
     "name": "user",
     "password": "password",
-    "email": "a@l.com",
+    "email": "alex@gmail.com",
 }
+
+user_json_altered = {
+    "name": "user",
+    "password": "password",
+    "email": "afonso@gmail.com",
+}
+
+user_object = UserLogin(
+    name=user_json.get("name"),
+    password=user_json.get("password"),
+    email=user_json.get("email")
+)
+
+user_object_altered = UserLogin(
+    name=user_json_altered.get("name"),
+    password=user_json_altered.get("password"),
+    email=user_json_altered.get("email")
+)
+
+user_object_with_email_invalid = UserLogin(
+    name=user_json.get("name"),
+    password=user_json.get("password"),
+    email="email@invalid"
+)
 
 
 class UserTestCase(TestCase):
-    def test_login_class(self):
-        login = UserLogin(
-            name=default_user.get("name"),
-            password=default_user.get("password"),
-            email=default_user.get("email")
-        )
-
-        self.assertEquals(default_user.get("name"), login.name)
-        self.assertEquals(default_user.get("password"), login.password)
-        self.assertEquals(default_user.get("email"), login.email)
-
-    def test_register_user_valid(self):
+    def test_user_class(self):
         setup_user = UserLogin(
-            name=default_user.get("name"),
-            password=default_user.get("password"),
-            email=default_user.get("email")
+            name=user_json.get("name"),
+            password=user_json.get("password"),
+            email=user_json.get("email")
         )
-
-        setup_user.register_user()
 
         self.assertEquals(
-            User.objects.get(
-                name=setup_user.name
-            ).name,
-            setup_user.name
-        )
+            setup_user.is_correct_password(user_json.get("password")), True)
+
+    def test_register_user_valid(self):
+        is_registred = user_object.register_user()
+
+        self.assertEquals(is_registred.get('success'), True)
+
+        user_registred = UserLogin.get_user_by_name(user_object.name)
+
+        self.assertEquals(user_registred.get('success'), True)
+        self.assertEquals(user_registred.get(
+            'user').get('name'), user_object.name)
+
+    def test_register_user_invalid(self):
+        is_registred = user_object_with_email_invalid.register_user()
+
+        self.assertEquals(is_registred.get('success'), False)
+        self.assertEquals(is_registred.get('user'), None)
+
+    def test_update_user(self):
+        is_registred = user_object.register_user()
+
+        self.assertEquals(is_registred.get('success'), True)
+
+        is_user_altered = user_object_altered.update_user()
+
+        self.assertEquals(is_user_altered.get("success"), True)
+        self.assertEquals(is_user_altered.get('user')
+                          .get('name'), user_object_altered.name)
+        self.assertEquals(is_user_altered.get('user')
+                          .get('email'), user_object_altered.email)
+
+    def test_password(self):
+        random_number = random.randint(10000000000, 90000000000)
+        ramdom_password = str(random_number)
+
+        user_object.password = UserLogin.encrypt_password(ramdom_password)
+
+        self.assertNotEquals(ramdom_password, user_object.password)
+        self.assertEquals(
+            user_object.is_correct_password(ramdom_password), True)
